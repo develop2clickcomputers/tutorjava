a1baff2c7be109adf5520270c61e2b4135719033
RNGokulStudio <123423028+RNGokulStudio@users.noreply.github.com>
2024-05-26 19:17:19 -0400

    Update GokulaKrishnan.java

--- GokulaKrishnan.java
+++ GokulaKrishnan.java
@@ -1,6 +1,7 @@
 public class GokulaKrishnan {
     public static void main(String[] args) {
         System.out.println("Hello Gokula Krishnan!!!");       
-        System.out.println("How are you?");       
+        System.out.println("How are you?");  
+        System.out.println("How old are you?");
     }
 }
